91aee5f8f8511a3ea71a0fd365b3e9cf84d3eb96
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-09 09:47:56 -0300

    a

--- src/validators/user/user.validatos.ts
+++ src/validators/user/user.validatos.ts
@@ -1,7 +1,8 @@
 import * as yup from "yup"
 
     const validationSchema = yup.object().shape({
-      cedula: yup.number().test('len', 'La cedula debe contener al menos 8 caracteres', (val:any) => val.toString().length === 8).required("Se requiere de este campo"),
+      cedula: yup.number().required("Se requiere de este campo").test('len', 'La cedula debe contener al menos 8 caracteres', (val:any) => val.toString().length === 8),
+      
     });
     const format = (err:any) => {
       const errors = [] as any
