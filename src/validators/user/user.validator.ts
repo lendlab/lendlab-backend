00a527cb68a212d5d02b82eaaff8d0e82cbb83da
nicoheredia59 <nicoheredia59@gmail.com>
2021-11-09 14:43:13 -0300

    a

--- src/validators/user/user.validator.ts
+++ src/validators/user/user.validator.ts
@@ -1,20 +1,13 @@
 import * as yup from "yup";
 
 const user = {
-  cedula: yup
-    .number()
-    .test(
-      "len",
-      "La cedula debe contener al menos ocho caracteres.",
-      (val: any) => val.toString().length
-    ),
+  cedula: yup.number(),
   password: yup
     .string()
-    .required("Debes ingresar una contraseña valida")
     .test(
       "len",
       "Tu contraseña debe tener al menos cinco caracteres.",
-      (val: any) => val.toString().length > 5
+      (val: any) => val.toString().length >= 5
     ),
 };
 
